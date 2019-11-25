f86f9dec37950dcf0dd80e0f6f7effc63e3abe15
smartmetropolisgabriel <j.quaresmasantos_98@hotmail.com>
2019-11-25 10:58:13 -0300

    Added Intent for Camera Activity

--- app/src/main/java/com/example/photospheremobile/views/MainActivity.kt
+++ app/src/main/java/com/example/photospheremobile/views/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.photospheremobile.views
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -27,8 +28,8 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = layoutManager
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
         }
     }
 
